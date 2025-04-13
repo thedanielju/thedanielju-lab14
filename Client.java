ee1ed89963bd921181d40d2b4834b82cdc28c8da
thedanielju <danjuacademic@gmail.com>
2025-04-13 15:23:37 -0400

    pretest client class

--- Client.java
+++ Client.java
@@ -17,4 +17,35 @@ public class Client{
         out = new PrintWriter(socket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
     }
+
+    public boolean sendHandshake() throws IOException {
+        final int handshake = 12345;
+        out.println(handshake);
+        String response = in.readLine();
+        if (response != null && response.contains("couldn't handshake")) {
+            System.out.println("Handshake failed: " + response);
+            return false;
+        }
+        System.out.println("Connected to server successfully");
+        return true;
+    }
+
+    public String factorize(long number) throws IOException {
+        out.println(number);
+
+        String response = in.readLine();
+        return response;
+    }
+
+    public void disconnect() throws IOException {
+        try {
+            if (in != null) in.close();
+            if (out != null) out.close();
+            if (socket != null && !socket.isClosed()) socket.close();
+            System.out.println("Disconnected from server");
+        } catch (IOException e) {
+            System.err.println("Error while disconnecting: " + e.getMessage());
+            throw e;
+        }
+    }
 }
